f3e207918b8055ce6e3ed46cc49f07f316fd1b22
wyl <wangyalei95@163.com>
2022-06-05 10:37:07 +0800

    master commit3

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello World3!");
         System.out.println("Hello World hot-fix!");
         System.out.println("Hello World master!");
+        System.out.println("Hello World git push!");
     }
 }
